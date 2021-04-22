933e26fba7af076e58e79254885a418e52c27f1b
Fernando Augusto <fernando.augustogomesp@gmail.com>
2021-04-22 19:30:24 -0300

    Test for "check-keys.js"

---
+++ tests/oauth/t-check-keys.js
@@ -0,0 +1,19 @@
+const isEmptyKeys = require('../../oauth/check-keys');
+const assert = require('assert');
+
+describe('Test Errors whether there are a invalid key', function() {
+	it('Check Consumer Key', function() {
+		assert.throws(isEmptyKeys, Error);
+	});
+});
+
+describe('Test if everything goes fine', function() {
+	it('Valid keys', function() {
+		// load env vars
+		require('dotenv').config({
+			path: '../../.env'
+		});
+		
+		isEmptyKeys();
+	});
+});
