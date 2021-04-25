b5e9c5cab04bfe6b8ddc6d4e39a867f7f23f4ab2
Fernando Augusto <fernando.augustogomesp@gmail.com>
2021-04-24 21:17:03 -0300

    Oauth to talk with twitter api

---
+++ oauth/oauth.js
@@ -0,0 +1,32 @@
+const { OAuth } = require('oauth')
+const isEmptyTokens = require('./check-keys')
+const { promisify } = require('util')
+const TwitterError = require('./twitter-error.js')
+
+async function requestOAuth (URI) {
+  try {
+    isEmptyTokens()
+  } catch (invalidTokens) {
+    return new TwitterError(invalidTokens)
+  }
+
+  const oauth = new OAuth(
+    'https://api.twitter.com/oauth/request_token',
+    'https://api.twitter.com/oauth/access_token',
+    process.env.CONSUMER_KEY,
+    process.env.CONSUMER_SECRET,
+    '1.0A', null, 'HMAC-SHA1'
+  )
+
+  const get = promisify(oauth.get.bind(oauth))
+
+  const responseData = await get(
+    URI,
+    process.env.ACCESS_KEY,
+    process.env.ACCESS_TOKEN_SECRET
+  )
+
+  return JSON.parse(responseData)
+}
+
+module.exports = requestOAuth
