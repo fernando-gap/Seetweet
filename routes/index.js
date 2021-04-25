68d30d65f66cbf0fb74c75b3276c9754ad56adcf
Fernando Augusto <fernando.augustogomesp@gmail.com>
2021-04-24 21:07:23 -0300

    change name of redirect to filter

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 
 // configure routes
-router.use('/', require('./requests/search/redirect'))
+router.use('/', require('./requests/search/filter'))
 router.use('/search', require('./requests/search/tweets'))
 
 module.exports = router
