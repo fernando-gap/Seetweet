1fe040e07e299c668028dd636618a6a3b0e03a8a
Fernando Augusto <fernando.augustogomesp@gmail.com>
2021-04-24 21:04:52 -0300

    absolute path added to express static files

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require('express')
+const { join } = require('path')
 
 // global object
 const app = express()
 
 // routes
-app.use(require('./routes'))
+app.use(require(join(__dirname, '/routes')))
 
 // load search/tweets frontend
-// app.use('/search/tweets', express.static('./frontend/search/'));
-app.use('/search/tweets', express.static('./frontend/search/'))
+app.use('/search/tweets', express.static(
+  join(__dirname, '/frontend/search/'
+)))
 
 // load dependencies for project
-app.use('/code', express.static('./frontend/'))
+app.use('/code', express.static(join(__dirname, '/frontend/')))
 
 app.listen(8080, () => {
   console.log('Server started! host::%s', 'http://localhost:8080')
