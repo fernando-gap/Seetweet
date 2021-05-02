72ed7132ad6a1b5b8addb83ea179ea9ca0e8a31b
Fernando <fernando.augustogomesp@gmail.com>
2021-05-01 22:49:31 -0300

    Handle erros 404 and 405

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const app = express()
 
 // routes
 app.use(require(join(__dirname, '/routes')))
+app.use(function(err, req, res, next) {
+  res.status(405)
+  res.render('error/http', {
+    layout: 'main.hbs',
+    code: 405,
+    error: 'Method not allowed'
+  })
+  console.log('Method not allowed')
+})
 
 // load search/tweets frontend
 app.use('/search/tweets', express.static(
@@ -18,3 +27,14 @@ app.use('/code', express.static(join(__dirname, '/frontend/')))
 app.listen(8080, () => {
   console.log('Server started! host::%s', 'http://localhost:8080')
 })
+
+
+// handle 404 errors
+app.use(function(req, res, next) {
+  res.status(404)
+  res.render('error/http', { 
+    layout: 'main.hbs',
+    code: 404,
+    error: 'Not Found'
+  })
+});
